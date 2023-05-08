32754b0ade496fd8dec4350af354f7d2c9ebee16
Kubra Guran <kubraguran00@gmail.com>
2023-05-08 15:34:42 +0200

    uptodate

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react';
-import ReactPlayer from 'react-player';
 import ParrotiasLogo from '../images/Parrotias_logo.png';
 
 const Video = ({ videos }) => {
   const [video, setVideo] = useState(null);
-  const [loop, setLoop] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isMuted, setIsMuted] = useState(true);
 
   const sortedVideos = useRef();
   useEffect(() => {
@@ -18,34 +17,33 @@ const Video = ({ videos }) => {
 
     setVideo(sortedVideos.current[0]);
   }, [videos]);
+
   function goToNextVideo(sortedVideos) {
-    // Increment the current index and loop back to 0 if we've reached the end
-    console.log(sortedVideos.length);
     const nextIndex = (currentIndex + 1) % sortedVideos.length;
     setCurrentIndex(nextIndex);
-
-    // Set the video state to the next URL in the sorted array
     setVideo(sortedVideos[nextIndex]);
   }
+
+  function toggleMute() {
+    setIsMuted(!isMuted);
+  }
+
   return (
     <div className='video'>
-      {/* <ReactPlayer
-        loop={loop}
-        url={video}
-        playing={true}
-        controls={true}
-        muted={true}
-        light={false}
-        onEnded={() => goToNextVideo(sortedVideos.current)}
-      /> */}
       <video
         src={video}
         autoPlay
-        muted
-        controls
-        poster={ParrotiasLogo}
+        muted={isMuted}
         onEnded={() => goToNextVideo(sortedVideos.current)}
       ></video>
+      <button onClick={toggleMute}>
+        mute
+        {isMuted ? (
+          <i className="fas fa-volume-mute"></i>
+        ) : (
+          <i className="fas fa-volume-up"></i>
+        )}
+      </button>
     </div>
   );
 };
